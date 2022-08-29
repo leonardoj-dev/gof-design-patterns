1fe8b43f921d64927136cd2d097a57496409599a
leonardoj <ljaime@bdsol.com.ar>
2022-08-29 01:37:52 -0300

    summary

--- src/main/java/org/example/gof/Application.java
+++ src/main/java/org/example/gof/Application.java
@@ -3,6 +3,9 @@ package org.example.gof;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Summary
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
